2b48d6f42dbc13db8ebcd8e3d406bc93451f8029
Beimnet <beimnetbekele123@gmail.com>
2024-09-09 15:36:47 +0300

    updated authorization for orgMember on orgAdmin router

--- Backend/routes/orgAdminRoutes.js
+++ Backend/routes/orgAdminRoutes.js
@@ -5,13 +5,13 @@ const router = require('express').Router()
 
 router.route('/create_org_member').post(authentication,restrictTo("OrgAdmin"),createOrgMember)
 router.route('/create_form').post(authentication,restrictTo("OrgAdmin"),createForm)
-router.route('/get-forms').get(authentication,restrictTo("OrgAdmin"),getFormsByOrganization)
-router.route('/get-responses/:formId').get(authentication,restrictTo("OrgAdmin"),getResponsesByForm)
-router.route('/get-form-questions/:formId').get(authentication,restrictTo("OrgAdmin"),getFormQuestions)
+router.route('/get-forms').get(authentication,restrictTo("OrgAdmin", "OrgMember"),getFormsByOrganization)
+router.route('/get-responses/:formId').get(authentication,restrictTo("OrgAdmin", "OrgMember"),getResponsesByForm)
+router.route('/get-form-questions/:formId').get(authentication,restrictTo("OrgAdmin", "OrgMember"),getFormQuestions)
 
-router.route('/get-form-response-stat/:formId/').get(authentication,restrictTo("OrgAdmin"),getResponseStatsByForm);
-router.route('/get-organization-response-stat/').get(authentication,restrictTo("OrgAdmin"), getResponseStatsByOrganization);
-router.route('/validate-response/:responseId').patch(authentication,restrictTo("OrgAdmin"), validateResponse);
+router.route('/get-form-response-stat/:formId/').get(authentication,restrictTo("OrgAdmin", "OrgMember"),getResponseStatsByForm);
+router.route('/get-organization-response-stat/').get(authentication,restrictTo("OrgAdmin", "OrgMember"), getResponseStatsByOrganization);
+router.route('/validate-response/:responseId').patch(authentication,restrictTo("OrgAdmin", "OrgMember"), validateResponse);
 
 
 module.exports = router
